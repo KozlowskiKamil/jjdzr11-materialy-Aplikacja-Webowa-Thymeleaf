12874d96c937a31f546b886dd39779cc02b2776d
Paweł Dobrzański <p.dobrzanski@yaerius.eu>
2023-05-07 17:08:04 +0200

    Exercise 2

--- src/main/java/com/infoshareacademy/thymeleaf/controller/MainController.java
+++ src/main/java/com/infoshareacademy/thymeleaf/controller/MainController.java
@@ -1,18 +1,30 @@
 package com.infoshareacademy.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static com.infoshareacademy.thymeleaf.model.Constants.*;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    String index() {
+    String index(Model model) {
+        model.addAttribute("dateTime",
+            LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+        );
         return "index";
     }
 
     @GetMapping("/info")
-    String info() {
+    String info(Model model) {
+        model.addAttribute("name", INFO_NAME)
+             .addAttribute("role", INFO_ROLE)
+             .addAttribute("slack", INFO_SLACK);
         return "info";
     }
 
---
+++ src/main/java/com/infoshareacademy/thymeleaf/model/Constants.java
@@ -0,0 +1,7 @@
+package com.infoshareacademy.thymeleaf.model;
+
+public class Constants {
+    public static final String INFO_NAME = "Paweł Dobrzański";
+    public static final String INFO_ROLE = "Software Developer";
+    public static final String INFO_SLACK = "Slack: Pawel.Dobrzanski";
+}
