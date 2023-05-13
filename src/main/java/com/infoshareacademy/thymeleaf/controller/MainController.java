6c29c741e4d94933a8879df71643b5d565ca624b
Paweł Dobrzański <p.dobrzanski@yaerius.eu>
2023-05-13 23:39:40 +0200

    Exercise 4 (adds custom.css)

--- src/main/java/com/infoshareacademy/thymeleaf/controller/MainController.java
+++ src/main/java/com/infoshareacademy/thymeleaf/controller/MainController.java
@@ -20,24 +20,26 @@ public class MainController {
     @GetMapping("/")
     String index(Model model) {
         model.addAttribute("dateTime",
-            LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATE_TIME))
-        );
-        return "index";
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATE_TIME)))
+             .addAttribute("content", "index");
+        return "main";
     }
 
     @GetMapping("/info")
     String info(Model model) {
         model.addAttribute("name", INFO_NAME)
              .addAttribute("role", INFO_ROLE)
-             .addAttribute("slack", INFO_SLACK);
-        return "info";
+             .addAttribute("slack", INFO_SLACK)
+             .addAttribute("content", "info");
+        return "main";
     }
 
     @GetMapping("/players")
     String players(Model model) {
         model.addAttribute("player1", playerService.getPlayer(1))
-             .addAttribute("player2", playerService.getPlayer(2));
-        return "players";
+             .addAttribute("player2", playerService.getPlayer(2))
+             .addAttribute("content", "players");
+        return "main";
     }
 
 }
